6d4e8c07b3599e6123a6ffe82145665c96113396
Noon Silk <noonsilk@gmail.com>
2013-12-12 16:54:03 +1100

    prints what I am doing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative
+-- import           Control.Applicative
 import           Snap.Core
-import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           Network.HTTP.Conduit
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as L
+import           Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+    ifTop homeHandler
+    -- <|>
+    -- route [ ("foo", writeBS "bar")
+    --       , ("echo/:echoparam", echoHandler)
+    --       ]
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+
+homeHandler :: Snap ()
+homeHandler = do
+    -- Bytestrings 
+    --  https://www.fpcomplete.com/school/to-infinity-and-beyond/pick-of-the-week/bytestring-bits-and-pieces
+    let url = "https://dl.dropboxusercontent.com/u/28596024/taskwarrior.txt" 
+    raw <- liftIO $ simpleHttp url
+    let project = B.concat $ L.toChunks raw
+    writeBS "<title>what is noon doing</title>"
+    writeBS project 
+
+
+-- echoHandler :: Snap ()
+-- echoHandler = do
+--     -- liftIO $ simpleHttp "http://www.example.com/foo.txt" >>= L.writeFile "foo.txt"
+--     param <- getParam "echoparam"
+--     maybe (writeBS "must specify echo/param in URL")
+--           writeBS param
